b7fe54f417943b5e99a8f2a86b4c0287c486482c
Mateusz Rusnak <mateusz@rusnak.biz>
2022-08-01 20:33:14 +0200

    Moved importing file to INPUT constructor and cleaned code in INPUT.

--- src/main/java/org/example/Input.java
+++ src/main/java/org/example/Input.java
@@ -10,13 +10,10 @@ public class Input {
     private String[] allWords;
 
     public Input() {
-        importWordsFromFile();
-    }
-
-    private void importWordsFromFile() {
         this.allWords = new String[0];
         File wordsFile = new File("Words.txt");
         Scanner fileReader = null;
+
         while (fileReader == null) {
             try {
                 fileReader = new Scanner(wordsFile);
@@ -25,20 +22,23 @@ public class Input {
                 doPlayerWantsToTryAgain();
             }
         }
+
         int wordsArrayIndex = 0;
+
         while (fileReader.hasNextLine()) {
             String line = fileReader.nextLine();
             this.allWords = Arrays.copyOf(this.allWords, this.allWords.length + 1);
             this.allWords[wordsArrayIndex] = line;
             ++wordsArrayIndex;
         }
+
         fileReader.close();
     }
 
     public int chooseBetweenTwoOptions() {
-        //TODO clean - did like this because of problems with .nextInt()
         int option = 0;
         String keys;
+
         while (option != 1 && option != 2) {
             System.out.print("Choose by typing 1 or 2, then press Enter: ");
             keys = keyboardInput.nextLine();
@@ -48,6 +48,7 @@ public class Input {
                 System.out.print("Wrong value. ");
             }
         }
+
         return option;
     }
 
@@ -55,26 +56,30 @@ public class Input {
         System.out.println("There are two difficulty levels in this game:");
         System.out.println("1. EASY - 4 randomly selected word pairs to discover and 10 chances to reveal all memory");
         System.out.println("2. HARD - 8 randomly selected word pairs and 15 chances to reveal all word pairs ");
-        return chooseBetweenTwoOptions(); //TODO try to use char
+        return chooseBetweenTwoOptions();
     }
 
     public String provideCoordinates(String[] validCoordinatesArray) {
         boolean validCoordinatesProvided = false;
         String coordinates = "";
+
         while (!validCoordinatesProvided) {
             System.out.println("Valid options: " + Arrays.toString(validCoordinatesArray));
             System.out.print("Type coordinates to check and press Enter: ");
             coordinates = keyboardInput.nextLine();
+
             for (String coordinate : validCoordinatesArray) {
                 if (coordinate.equals(coordinates)) {
                     validCoordinatesProvided = true;
                     break;
                 }
             }
+
             if (!validCoordinatesProvided) {
                 System.out.print("Wrong value. ");
             }
         }
+
         return coordinates;
     }
 
